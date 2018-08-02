0a1510900ad9b31e0e45e2965e14d58d73d77f01
unknown <yoke-msn@hotmail.com>
2018-08-02 11:24:29 +0800

    Add hacks to ignore https untrust cert validation for invoke-restmethod

--- functions/PhpIpamBaseFunctions.ps1
+++ functions/PhpIpamBaseFunctions.ps1
@@ -1,4 +1,22 @@
 ﻿
+# Function to disable checking untrust cert
+# if phpipam was running on https and using untrust cert ,invoke-restMethod can not handle this
+function disable-CertsCheck {
+    add-type -TypeDefinition  @"
+        using System.Net;
+        using System.Security.Cryptography.X509Certificates;
+        public class TrustAllCertsPolicy : ICertificatePolicy {
+            public bool CheckValidationResult(
+                ServicePoint srvPoint, X509Certificate certificate,
+                WebRequest request, int certificateProblem) {
+                return true;
+            }
+        }
+"@
+    [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+}
+
+disable-CertsCheck
 function Protect-Rijndael256ECB {
     param(
         [string]$Key,
