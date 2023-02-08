90d963484ef889b74447828b6e9a7a9903a847f5
yoke88 <yoke-msn@hotmail.com>
2023-02-08 16:10:08 +0800

    !deploy it fix Get-PhpIpamSubnets

--- PSPHPIPAM/Functions/Public/Subnets/Get-PhpIpamSubnets.ps1
+++ PSPHPIPAM/Functions/Public/Subnets/Get-PhpIpamSubnets.ps1
@@ -3,7 +3,8 @@ function Get-PhpIpamSubnets{
     param(
         
     )
-    Invoke-PhpIpamExecute -method get -controller subnets
+    $r=Invoke-PhpIpamExecute -method get -controller subnets
+    Resolve-PhpIpamExecuteResult -result $r
 }
 new-alias -Name Get-PhpIpamAllSubnets -Value Get-PhpIpamSubnets
 Export-ModuleMember -Function Get-PhpIpamSubnets -Alias Get-PhpIpamAllSubnets
